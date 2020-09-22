17fbe842396b4f36d5654c26e2fa95eb19af2a88
Miguel <abbetto4@gmail.com>
2020-09-22 18:47:44 +0200

    messaggio di testo cambiato

---
+++ app/src/main/java/sanchez/miguel/alfonso/simul/MainActivity.java
@@ -0,0 +1,18 @@
+package sanchez.miguel.alfonso.simul;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView testo = findViewById(R.id.testo_principale);
+        testo.setText("Buonasera! gii");
+    }
+
+
+}
