ee85b8969c4f90add582f948ca3f2be162963368
martintodorovunive <martintodorovunive@gmail.com>
2020-11-13 16:01:09 +0100

    Integrazione parte front-end navigazione e home page (e altra roba) (update ui e listener delete account e logout commentati per contrasti) - Files

---
+++ app/src/main/java/sanchez/miguel/alfonso/simul/Amici.java
@@ -0,0 +1,27 @@
+package sanchez.miguel.alfonso.simul;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Amici extends Fragment {
+
+    public Amici() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_amici, container, false);
+    }
+}
---
+++ app/src/main/java/sanchez/miguel/alfonso/simul/Impostazioni.java
@@ -0,0 +1,27 @@
+package sanchez.miguel.alfonso.simul;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Impostazioni extends Fragment {
+
+    public Impostazioni() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_impostazioni, container, false);
+    }
+}
